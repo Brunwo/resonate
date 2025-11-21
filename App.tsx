3c409f3d722d0457524cc29d4dfb5e8fe3858b22
Brunwo <bwagner57@gmail.com>
2025-11-21 23:55:12 +0100

    Fix TypeScript unused variable errors

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { SOUNDS } from './constants';
 import { SoundCard } from './components/SoundCard';
 import { CustomBuilder } from './components/CustomBuilder';
